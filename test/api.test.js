92725c6e8d41cf0bad3bc86a0ff521b80d4593d7
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-13 10:09:54 -0800

    added initial connection before hook and test for empty array

--- test/api.test.js
+++ test/api.test.js
@@ -1,5 +1,34 @@
-const assert = require('chai').assert;
-const ripeBananas = require('../lib/models');
+const chai = require('chai');
+const assert = chai.assert;
+const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
+const app = require('../lib/app');
 
-mongoose.Promise = Promise;
+chai.use(chaiHttp);
+const request = chai.request(app);
+
+process.env.MONGODB_URI = 'mongodb://localhost:27017/ripeBananas-test';
+require('../lib/mongo-connection');
+
+describe('actors API TEST', () => {
+
+    before(() => mongoose.connection.dropDatabase());
+
+    it('GET returns empty array of actors', () => {
+        return request.get('/actors')
+            .then(req => req.body)
+            .then(res => {
+                assert.deepEqual(res, []);
+            })
+    });
+
+    // it('POST all actors', () => {
+       
+    // });
+
+    // it('GET one actor by id', () => {
+       
+    // });
+
+});
+
