f779b15f3ba7995e421778d8a8371f950ca5c807
Brigitte Huneke <brigittehuneke@gmail.com>
2017-02-13 10:53:43 -0800

    renamed database test

--- test/database.test.js
+++ test/database.test.js
@@ -1,11 +1,5 @@
-const chai = require('chai');
-const assert = chai.assert;
-const chaiHttp = require('chai-http');
+const assert = require('chai').assert;
+//const ripeBananas = require('../lib/models');
 const mongoose = require('mongoose');
-const app = require('../lib/app');
 
-chai.use(chaiHttp);
-const request = chai.request(app);
-
-process.env.MONGODB_URI = 'mongodb://localhost:27017/ripeBananas-test';
-require('../lib/mongo-connection');
+mongoose.Promise = Promise;
