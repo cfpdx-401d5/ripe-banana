29fc49eae3a51652bd4251575d17e7bfb711c1b3
sdcaulley <stephanie.caulley@gmail.com>
2017-02-21 15:23:21 -0800

    updated server code with env

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const app = require('./lib/app');
 const http = require('http');
 
 require('./lib/mongo-connection');
-
+const PORT = process.env.PORT || 3000;
 const server = http.createServer(app);
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log('server running on', server.address());
 });
